347d05fa341ca4260ba44659a7714c11b3980020
Andre Costa <mr.luizandre@gmail.com>
2023-09-24 13:30:13 -0300

    docs: add documentation for listOpening

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -7,6 +7,16 @@ import (
 	"github.com/mrluizandre/gopportunities/schemas"
 )
 
+// @Basepath /api/v1
+
+// @Summary List Openings
+// @Description List all job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningsHandler(context *gin.Context) {
 	openings := []schemas.Opening{}
 
