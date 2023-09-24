ab49bd9b2941ce3b193786d88366442972edafa3
Andre Costa <mr.luizandre@gmail.com>
2023-09-24 11:29:13 -0300

    feat: add request & reponse helpers

---
+++ handler/response.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func sendError(context *gin.Context, code int, msg string) {
+	context.Header("Content-Type", "application/json")
+	context.JSON(code, gin.H{
+		"message":   msg,
+		"errorCode": code,
+	})
+}
+
+func sendSuccess(context *gin.Context, op string, data interface{}) {
+	context.Header("Content-Type", "application/json")
+	context.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation from handler: %s successfull", op),
+		"data":    data,
+	})
+}
